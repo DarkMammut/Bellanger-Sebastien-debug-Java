64de880746b3650acb3d3bdc11a86932a585496b
Sébastien BELLANGER <seb.bellanger@gmail.com>
2023-12-21 20:56:24 +0100

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataFromFile.java
@@ -0,0 +1,30 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+// Classe implémentant l'interface pour écrire les symptômes dans un fichier
+class WriteSymptomDataToFile implements ISymptomWriter {
+    private final String filepath;
+
+    public WriteSymptomDataToFile(String filepath) {
+        this.filepath = filepath;
+    }
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                String symptom = entry.getKey();
+                int quantity = entry.getValue();
+                writer.write(symptom + ": " + quantity + "\n");
+            }
+			writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
